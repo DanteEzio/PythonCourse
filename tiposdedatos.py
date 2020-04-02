1357377c624a7dec1ff9b5744f7c9a460e19a616
Dante <quantum120@hotmail.com>
2020-04-02 12:07:59 -0600

    Creamos un programa para sumar dos numeros

---
+++ tiposdedatos.py
@@ -0,0 +1,31 @@
+#print("Hello, world")
+
+entero = 15
+flotantes = 5.32151
+string = "Texto"
+booleano = True
+
+#Operadores artimeticos  + - / *
+#Tipos de datos
+suma = 4 + 5
+resta = 5 - 4
+multiplicacion = 5 * 4
+potencia = 5 ** 2 #Numero al cuadrado
+division = 10 / 2
+division_exacta = 5 // 2 #Solo nos regresa enteros
+modulo = 10 % 2 #Nos muestra el residuo de una division por ejemplo aqui seria 0
+
+#nos muestra el resultado de la variable
+#print('El valor de tu suma es:', suma)
+
+# + Solo permite concatenar texto
+# , Permite concatenar cualquier tipo de dato
+
+numero_1 = float(input('Ingresa el primer valor:'))
+
+numero_2 = float(input('Ingresa el segundo valor:'))
+
+total = numero_1 * numero_2
+
+print('El total de la multiplicacion es de:','{:0.3}'.format(total))
+
